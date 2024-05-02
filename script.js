c710a0b9bc1add234ddf71f3813455320de6d3cd
rafaseoanes <rafaseoanes9@gmail.com>
2024-05-01 20:42:00 -0500

    Delete button functionality implemented

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const openForm = document.getElementById("openForm");
 const closeModalButton = document.getElementById("closeModalButton");
 const addBook = document.getElementById("addBook");
 
-const deleteBook = document.getElementsByClassName("deleteButton");
-
 //form inputs:
 
 const title = document.getElementById("title");
@@ -14,24 +12,27 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const read = document.getElementById("read");
 
-const myLibrary = [
+let myLibrary = [
   {
     title: "The little prince",
     author: "Antoine de Saint-Exupéry",
     pages: 96,
     read: true,
+    id: 0,
   },
   {
     title: "IT",
     author: "Stephen King",
     pages: 1504,
     read: false,
+    id: 1,
   },
   {
     title: "How to Catch a Dinosaur",
     author: "Adam Wallace",
     pages: 40,
     read: false,
+    id: 2,
   },
 ];
 
@@ -46,42 +47,43 @@ function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
 }
 
-function displayBooks(library) {
-  cardContainer.innerHTML = "";
-  library.forEach((book) => {
-    const carDiv = document.createElement("div");
-    carDiv.classList.add("card");
-    cardContainer.appendChild(carDiv);
+function displayBooks(book) {
+  const carDiv = document.createElement("div");
+  carDiv.classList.add("card");
+  cardContainer.appendChild(carDiv);
 
-    const title = document.createElement("h3");
-    title.innerText = book.title;
-    carDiv.appendChild(title);
+  const title = document.createElement("h3");
+  title.innerText = book.title;
 
-    const author = document.createElement("p");
-    author.innerText = `By: ${book.author}`;
-    carDiv.appendChild(author);
+  const author = document.createElement("p");
+  author.innerText = `By: ${book.author}`;
 
-    const pages = document.createElement("p");
-    pages.innerText = `Pages: ${book.pages}`;
-    carDiv.appendChild(pages);
+  const pages = document.createElement("p");
+  pages.innerText = `Pages: ${book.pages}`;
 
-    const read = document.createElement("p");
+  const read = document.createElement("p");
 
-    if (book.read) {
-      read.innerText = "Read?: ✅";
-    } else {
-      read.innerText = "Read?: ⛔";
-    }
-    carDiv.appendChild(read);
+  if (book.read) {
+    read.innerText = "Read?: ✅";
+  } else {
+    read.innerText = "Read?: ⛔";
+  }
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("deleteButton");
-    deleteButton.innerText = "Delete";
-    carDiv.appendChild(deleteButton);
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("deleteButton");
+  deleteButton.innerText = "Delete";
+
+  deleteButton.addEventListener("click", (e) => {
+    myLibrary = myLibrary.filter((currentBook) => {
+      return currentBook.id !== book.id;
+    });
+    e.target.parentElement.remove();
+
+    // console.log("borraste", myLibrary)
   });
-}
 
-displayBooks(myLibrary);
+  carDiv.append(title, author, read, pages, deleteButton);
+}
 
 openForm.addEventListener("click", () => {
   userInputDialog.showModal();
@@ -99,10 +101,20 @@ addBook.addEventListener("click", (event) => {
     read.checked
   );
 
-  newBook.index = myLibrary.length;
+  newBook.id = myLibrary.length;
+
   addBookToLibrary(newBook);
-  displayBooks(myLibrary);
+  displayBooks(myLibrary[newBook.id]);
+
+  // console.log(myLibrary[newBook.id])
+  console.log(myLibrary);
 
   userInputDialog.close();
   event.preventDefault();
 });
+
+function showPlaceholders(myLibrary) {
+  myLibrary.forEach((element) => displayBooks(element));
+}
+showPlaceholders(myLibrary);
+console.log(myLibrary);
